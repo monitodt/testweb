89f3abfdd5f4789f7d590fe98e71bd23ecade286
kostya <kostyan20000607@icloud.com>
2020-12-16 11:45:47 +0500

    Diploy

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import dj-database-url
+
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +81,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj-database-url
 db_from_env = dj-database-url.config()
 DATABASE['default'].update(db_from_env)
 
